8c86c3d2bd1633a779482c9fb28e12fd834afb51
Fournier Axel <axelfournier78@gmail.com>
2020-03-17 18:28:19 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Grid :height="20" :width="20" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Grid from '@/components/Grid.vue';
+
+@Component({
+  components: {
+    Grid,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import "bulma";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
