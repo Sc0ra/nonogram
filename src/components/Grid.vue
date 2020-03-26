e682f1208bc72166ba643b7616e6424f9d0c7384
fournier_axe <axel.fournier@supelec.fr>
2020-03-26 23:50:52 +0100

    column and line inidications

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,19 +1,28 @@
 <template>
   <div @contextmenu.prevent="count+=1">
     <table class="table is-bordered">
+      <tr>
+        <th />
+        <th v-for="j in width" :key="j">
+          {{ columnIndications[j-1] }}
+        </th>
+      </tr>
       <tr
         v-for="(row, i) in state"
         :key="i"
       >
+        <td>
+          {{ lineIndications[i] }}
+        </td>
         <td
           v-for="(cell, j) in row"
           :key="j"
           class="cell"
-          :class="{'is-dark': cell === 'selected'}"
-          @mousedown="isEditing = true; updateCell(i, j, 'selected')"
-          @mouseup="isEditing = false"
-          @mouseenter="isEditing && updateCell(i, j, 'selected')"
-          @contextmenu.prevent="updateCell(i, j, 'flagged')"
+          :class="{'is-dark': cell === 1, 'is-danger': cell === 2}"
+          @mousedown.left="isEditing = true; updateCell(i, j, 1)"
+          @mouseup.left="isEditing = false"
+          @mouseenter.left="isEditing && updateCell(i, j, 1)"
+          @mouseup.right="cell === 0 ? updateCell(i, j, 2) : updateCell(i, j, 0)"
         >
         </td>
       </tr>
@@ -38,15 +47,39 @@ export default class Grid extends Vue {
   })
   width!: number;
 
-  private state: string[][] = [];
+  private state: number[][] = [];
 
   private isEditing = false;
 
-  public created() {
-    this.state = Array(this.height).fill('').map(() => Array(this.width).fill('').map(() => ''));
+  public lineIndication = (line: number[]) => {
+    const accumulator: number[] = [];
+    let count = 0;
+    return line.reduce((acc, curr) => {
+      if (curr === 1) {
+        count += 1;
+      } else if (count > 0) {
+        acc.push(count);
+        count = 0;
+      }
+      return acc;
+    }, accumulator);
   }
 
-  public updateCell(row: number, column: number, value: string) {
+  get lineIndications() {
+    return this.state.map(this.lineIndication);
+  }
+
+  get columnIndications() {
+    return [...Array(this.width).keys()]
+      .map((columnIndex) => this.state.map((line) => line[columnIndex]))
+      .map(this.lineIndication);
+  }
+
+  public created() {
+    this.state = Array(this.height).fill(0).map(() => Array(this.width).fill(0).map(() => 0));
+  }
+
+  public updateCell(row: number, column: number, value: number) {
     const newRow = this.state[row];
     newRow[column] = value;
     this.state.splice(row, 1, newRow);
