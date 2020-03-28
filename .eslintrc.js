839c05679d5ff5edd5d477ab053fc70a90b2e6ce
fournier_axe <axel.fournier@supelec.fr>
2020-03-28 14:11:03 +0100

    change eslint to recommended rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,29 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'linebreak-style': 0
   },
+
+  'extends': [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+    '@vue/typescript/recommended'
+  ]
 };
