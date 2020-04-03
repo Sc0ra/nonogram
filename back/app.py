d71fb3f3b6299a7f7a0a975b83a3376d65358a50
Fournier Axel <axelfournier78@gmail.com>
2020-04-03 18:57:33 +0200

    deploy on aws lambda for back (connection to mongoatlas for db)

--- back/app.py
+++ back/app.py
@@ -1,8 +1,10 @@
 import os
+import json
 import traceback
 
 from flask import Flask, jsonify
-from flask.logging import create_logger
+
+# from flask.logging import create_logger
 from flask_login import LoginManager, current_user
 from flask_pymongo import PyMongo
 
@@ -15,15 +17,16 @@ from core.exceptions import ApiException
 
 def create_app(testing=False):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
 
     # Load conf depending on the mode
     if testing:
-        app.config.from_pyfile("test.conf.py", silent=False)
+        app.config.from_pyfile("./instance/test.conf.py")
     elif app.env == "development":
-        app.config.from_pyfile("back.conf.py", silent=False)
+        app.config.from_pyfile("./instance/back.conf.py")
     elif app.env == "production":
-        app.config.from_pyfile("/etc/config/back.conf.py", silent=False)
+        app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+        app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
     else:
         raise ValueError("App mode unknow: not in dev|prod|test")
 
@@ -31,26 +34,26 @@ def create_app(testing=False):
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-    app.logger = create_logger(app)
+    # app.logger = create_logger(app)
 
     @login_manager.user_loader
     def load_user(user_id):
         return UserManager().get(user_id)
 
-    @app.errorhandler(ApiException)
-    def handle_api_exception(e):
-        app.logger.error(traceback.format_exc())
-        app.logger.error(e.internal_message)
-        response = jsonify(
-            {"error_code": e.error_code, "error_message": e.external_message,}
-        )
-        return response, e.status_code
+    # @app.errorhandler(ApiException)
+    # def handle_api_exception(e):
+    #     app.logger.error(traceback.format_exc())
+    #     app.logger.error(e.internal_message)
+    #     response = jsonify(
+    #         {"error_code": e.error_code, "error_message": e.external_message,}
+    #     )
+    #     return response, e.status_code
 
-    @app.errorhandler(Exception)
-    def handle_exception(e):
-        app.logger.error(traceback.format_exc())
-        response = jsonify({"error_code": -1, "error_message": "Unknown Error",})
-        return response, 500
+    # @app.errorhandler(Exception)
+    # def handle_exception(e):
+    #     app.logger.error(traceback.format_exc())
+    #     response = jsonify({"error_code": -1, "error_message": "Unknown Error",})
+    #     return response, 500
 
     app.register_blueprint(users)
     app.register_blueprint(levels)
@@ -59,11 +62,8 @@ def create_app(testing=False):
     def config():
         res = {
             "version": "0.0.1",
-            "isAuthenticated": current_user.is_authenticated,
             "env": app.env,
         }
-        if current_user.is_authenticated:
-            res.update({"currentUser": current_user.to_dict()})
         return jsonify(res)
 
     return app
