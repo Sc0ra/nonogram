5a9b7a1b92dbc0ff31d698bf1f25141b02597455
Fournier Axel <axelfournier78@gmail.com>
2020-04-03 19:13:40 +0200

    install vue s3 deploy

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,1 +1,23 @@
-module.exports = {};
+module.exports = {
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'eu-west-3',
+      bucket: 'nonogram-front',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5,
+    },
+  },
+};
